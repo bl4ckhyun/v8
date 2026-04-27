19b8e4cb5e838e786d3883b531765b3b07514dfb
Victor Gomes <victorgomes@chromium.org>
2026-04-27 15:32:54 +0200

    [codegen] Fix signed integer overflow in Assembler::GrowBuffer
    Compiling extremely large functions could cause `Assembler::GrowBuffer`
    to calculate a negative buffer size due to a signed integer overflow
    when doubling the size. This bypassed the out-of-memory guard and
    resulted in a massive heap-based buffer overflow.
    
    This CL fixes the issue by:
    1. Moving `kMaximalBufferSize` to `AssemblerBase` as a common constant.
    2. Adding a non-static helper method `ComputeNewBufferSize` to
       `AssemblerBase` that takes a `BufferGrowthStrategy` enum flag.
    3. Adding a `DCHECK` to ensure the new size fits within `int` boundaries.
    4. Updating `GrowBuffer` in arm, arm64, ia32, and x64 to use this helper.
    5. Adding a check against `kMaximalBufferSize` in
       `BaselineCompiler::AllocateBuffer` to prevent huge initial
       allocations.
    
    TAG=agy
    CONV=d58d11be-b7cb-4eab-a315-ddefcb616292
    
    Fixed: 506629455
    Change-Id: Iff3ee491f911b9183c9680db84417de9e89ed72d
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7796004
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Victor Gomes <victorgomes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#106882}

--- src/codegen/x64/assembler-x64.h
+++ src/codegen/x64/assembler-x64.h
@@ -2689,9 +2689,6 @@ class V8_EXPORT_PRIVATE Assembler : public AssemblerBase {
   static bool IsNop(Address addr);
   static bool IsJmpRel(Address addr);
 
-  // Avoid overflows for displacements etc.
-  static constexpr int kMaximalBufferSize = 512 * MB;
-
   uint8_t byte_at(int pos) { return buffer_start_[pos]; }
   void set_byte_at(int pos, uint8_t value) { buffer_start_[pos] = value; }
 
